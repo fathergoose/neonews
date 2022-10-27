6125f63f1fec70318ce9c1b3119687d9369d84b0
Al Ilseman <al.ilseman@augintel.us>
2022-10-27 16:54:19 -0500

    More advanced options

--- lua/neonews/init.lua
+++ lua/neonews/init.lua
@@ -1,40 +1,69 @@
 local neonews = {}
+local runtime_path = os.getenv("VIMRUNTIME")
 local defaults = {
 	data_file = vim.fn.stdpath("data") .. "/news_last_read",
-	news_file = "$VIMRUNTIME/doc/news.txt",
-    -- TODO: Confirm this works without alpha.nvim
-    check_on_startup = false,
+	news_file = runtime_path .. "/doc/news.txt",
+	datetime_format = "%Y-%m-%d %H:%M",
+	startup_message = false,
+	check_on_startup = true,
+	split = "",
 }
 function neonews.setup(opts)
-	neonews.config = opts and vim.tbl_deep_extend("force", defaults, opts) or defaults
-	if neonews.config.check_on_startup then
-		neonews.check_news()
-	end
+	neonews.config = (opts and vim.tbl_deep_extend("force", defaults, opts)) or defaults
 end
 
 function neonews.read_news()
-	vim.cmd("leftabove vert help news | exec '82wincmd|'")
+	vim.cmd("rightbelow vert help news | exec '82wincmd|'")
 	local now = vim.fn.strftime("%s")
-	vim.fn.writefile({ now }, neonews.config.data_file)
+	local result = vim.fn.writefile({ now }, neonews.config.data_file)
+	if result ~= 0 then
+		vim.notify("Failed to write news last read file", vim.log.levels.ERROR)
+	end
 end
 
 function neonews.mark_news_unread()
 	vim.fn.writefile({ "" }, neonews.config.data_file)
 end
 
-function neonews.check_news()
-	local news_updated_at = vim.fn.getftime(neonews.config.news_file)
-	if vim.fn.filereadable(neonews.config.data_file) == 1 then
-		local last_read = tonumber(vim.fn.readfile(neonews.config.data_file)[1])
+function neonews.check_news(quiet)
+	if quiet == nil then
+		quiet = false
+	end
+	local cfg = neonews.config
+	local news_updated_at = vim.fn.getftime(cfg.news_file)
+	if vim.fn.filereadable(cfg.data_file) == 1 then
+		local contents = vim.fn.readfile(cfg.data_file)
+		local last_read = tonumber(contents[1])
 		if last_read == nil or news_updated_at > last_read then
 			neonews.read_news()
+		elseif (news_updated_at ~= nil) and not quiet then
+			local last_read_friendly = vim.fn.strftime(cfg.datetime_format, last_read)
+			local news_updated_at_friendly = vim.fn.strftime(cfg.datetime_format, news_updated_at)
+			local message = "No new news since " .. news_updated_at_friendly .. "; last read at " .. last_read_friendly
+			vim.notify(message, vim.log.levels.INFO)
+		elseif news_updated_at == nil then
+			vim.notify("Failed to get last update timestamp for " .. cfg.news_file, vim.log.levels.ERROR)
 		end
 	else
-		vim.cmd("e " .. neonews.config.data_file)
+		vim.cmd("e " .. cfg.data_file)
 		vim.cmd("w | bw")
 		neonews.read_news()
 	end
 end
 
+if neonews.config and neonews.config.check_on_startup then
+	local augroup = vim.api.nvim_create_augroup("Neonews")
+	vim.api.nvim_create_autocmd("VimEnter", {
+		group = augroup,
+		callback = function()
+			if neonews.config.startup_message then
+				neonews.check_news()
+			else
+				neonews.check_news(true)
+			end
+		end,
+		once = true,
+	})
+end
 
 return neonews
--- plugin/neonews.vim
+++ plugin/neonews.vim
@@ -8,7 +8,15 @@ if exists("g:loaded_neonews")
 endif
 let g:loaded_neonews = 1
 
-command! ReadNews lua require'neonews'.read_news()
-command! UnreadNews lua require'neonews'.mark_news_unread()
-command! CheckNews lua require'neonews'.check_news()
+command! NNewsRead lua require'neonews'.read_news()
+command! NNewsMarkUnread lua require'neonews'.mark_news_unread()
 
+lua << EOF
+vim.api.nvim_create_user_command("NNewsCheck", function(params)
+	if (params.args == "false") or (params.args == "0") or (params.args == "quiet") or (params.args == "q") then
+		require("neonews").check_news(true)
+	else
+		require("neonews").check_news()
+	end
+end, { nargs = "?" })
+EOF
