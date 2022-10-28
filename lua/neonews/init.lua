9d8a3e20ae270cb0beb3b6b250bc74fdf1554dd7
Al Ilseman <al.ilseman@augintel.us>
2022-10-28 07:29:10 -0500

    Add option to check news.txt by sha256sum in addition to modified time

--- lua/neonews/init.lua
+++ lua/neonews/init.lua
@@ -1,12 +1,13 @@
+local fn = vim.fn
 local neonews = {}
 local runtime_path = os.getenv("VIMRUNTIME")
 local defaults = {
-	data_file = vim.fn.stdpath("data") .. "/news_last_read",
+	data_file = fn.stdpath("data") .. "/news_last_read",
 	news_file = runtime_path .. "/doc/news.txt",
 	datetime_format = "%Y-%m-%d %H:%M",
 	startup_message = false,
 	check_on_startup = true,
-	split = "",
+	unread_news_strategy = "timestamp", -- or "sha256"
 }
 function neonews.setup(opts)
 	neonews.config = (opts and vim.tbl_deep_extend("force", defaults, opts)) or defaults
@@ -14,15 +15,19 @@ end
 
 function neonews.read_news()
 	vim.cmd("rightbelow vert help news | exec '82wincmd|'")
-	local now = vim.fn.strftime("%s")
-	local result = vim.fn.writefile({ now }, neonews.config.data_file)
+	local hash = fn.sha256(fn.join(fn.readfile(neonews.config.news_file)))
+	local now = fn.strftime("%s")
+	local result = fn.writefile({
+		"timestamp " .. now,
+		"sha256 " .. hash,
+	}, neonews.config.data_file)
 	if result ~= 0 then
 		vim.notify("Failed to write news last read file", vim.log.levels.ERROR)
 	end
 end
 
 function neonews.mark_news_unread()
-	vim.fn.writefile({ "" }, neonews.config.data_file)
+	fn.writefile({ "" }, neonews.config.data_file)
 end
 
 function neonews.check_news(quiet)
@@ -30,19 +35,35 @@ function neonews.check_news(quiet)
 		quiet = false
 	end
 	local cfg = neonews.config
-	local news_updated_at = vim.fn.getftime(cfg.news_file)
-	if vim.fn.filereadable(cfg.data_file) == 1 then
-		local contents = vim.fn.readfile(cfg.data_file)
-		local last_read = tonumber(contents[1])
-		if last_read == nil or news_updated_at > last_read then
-			neonews.read_news()
-		elseif (news_updated_at ~= nil) and not quiet then
-			local last_read_friendly = vim.fn.strftime(cfg.datetime_format, last_read)
-			local news_updated_at_friendly = vim.fn.strftime(cfg.datetime_format, news_updated_at)
-			local message = "No new news since " .. news_updated_at_friendly .. "; last read at " .. last_read_friendly
-			vim.notify(message, vim.log.levels.INFO)
-		elseif news_updated_at == nil then
-			vim.notify("Failed to get last update timestamp for " .. cfg.news_file, vim.log.levels.ERROR)
+	local news_updated_at = fn.getftime(cfg.news_file)
+	local last_hash = fn.split(fn.readfile(cfg.data_file, "", 2)[2])[2]
+
+	if fn.filereadable(cfg.data_file) == 1 then
+		if cfg.unread_news_strategy == "timestamp" then
+			local last_read_at = fn.split(fn.readfile(cfg.data_file, "", 1)[1])[2]
+			if last_read_at == nil or last_read_at < news_updated_at then
+				neonews.read_news()
+			else
+				if not quiet and (news_updated_at ~= nil) then
+					local last_read_friendly = vim.fn.strftime(cfg.datetime_format, last_read)
+					local news_updated_at_friendly = vim.fn.strftime(cfg.datetime_format, news_updated_at)
+					local message = "No new news since "
+						.. news_updated_at_friendly
+						.. "; last read at "
+						.. last_read_friendly
+					vim.notify(message, vim.log.levels.INFO)
+				end
+			end
+		else -- cfg.unread_news_strategy == "sha256"
+			local latest_hash = fn.sha256(fn.join(fn.readfile(cfg.news_file)))
+			if last_hash ~= latest_hash then
+				neonews.read_news()
+			else
+				if not quiet and (news_updated_at ~= nil) then
+					local message = "No changes to news.txt since last checked - sha256: " .. last_hash
+					vim.notify(message, vim.log.levels.INFO)
+				end
+			end
 		end
 	else
 		vim.cmd("e " .. cfg.data_file)
